5677752190671883c8238782f84b9ff3f1253a56
jmarsh96 <pmyjm13@nottingham.ac.uk>
2020-01-24 16:04:29 +0000

    resolved conflicts

--- moons/mars.R
+++ moons/mars.R
@@ -4,9 +4,4 @@ print("hello")
 print("test")
 this is eugenio line
 made some changes on github website
-<<<<<<< HEAD
-joes updates
-=======
-
 This is Theo adding a line to see what happens
->>>>>>> 094382fc03a2be2b08f05cc010f04952c25c3b50
